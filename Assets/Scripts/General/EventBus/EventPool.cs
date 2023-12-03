c5f31063d77ef3778d969eab653124e4ce99ed66
Ilia Tereshkin <ilyatereshkin@gmail.com>
2023-12-03 19:41:27 +0600

    Init

---
+++ Assets/Scripts/General/EventBus/EventPool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class EventPool<T> where T : IGameEvent, new() {
+	
+	private Queue<T> _availableEvents;
+
+	public EventPool()
+	{
+		_availableEvents = new Queue<T>();
+	}
+
+	public T Get() {
+		if (_availableEvents.Count == 0) 
+		{
+			return new T();
+		}
+
+		return _availableEvents.Dequeue();
+	}
+
+	public void Release(T gameEvent) {
+		gameEvent.Dispose();
+		_availableEvents.Enqueue(gameEvent);
+	}
+}
---
+++ Assets/Scripts/General/EventBus/InitializationReadyEvent.cs
@@ -0,0 +1,9 @@
+﻿public class InitializationReadyEvent : IGameEvent
+{
+	public bool IsReady;
+	
+	public void Dispose()
+	{
+		IsReady = default;
+	}
+}
